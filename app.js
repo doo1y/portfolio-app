1f219d154561f28130407c7b7ab797b6094567ec
Samuel Park <s.park111497@gmail.com>
2023-05-10 17:00:38 -0700

    moved files around

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(express.static(`${__dirname}/frontend/build`));
 app.use("/data", dataRouter);
 
 app.listen(PORT, () => {
